c060e4867b4eb9d7ce30196d2c3af24cbf1de47e
nambp <nambp@eway.vn>
2019-10-20 11:05:31 +0700

    Add Config ActiveMQ

--- src/main/groovy/com/spring/multiple/mongo/datasource/springmultiplemongo/controller/Controller.groovy
+++ src/main/groovy/com/spring/multiple/mongo/datasource/springmultiplemongo/controller/Controller.groovy
@@ -4,6 +4,8 @@ import org.bson.Document
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.jms.core.JmsTemplate
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("")
 class Controller {
 
+    final String DESTINATION_NAME = "masoffer.test-message"
+
     @Autowired
     @Qualifier(value = "primaryMongoTemplate")
     MongoTemplate primaryMongoTemplate
@@ -21,10 +25,23 @@ class Controller {
     @Qualifier(value = "secondaryMongoTemplate")
     MongoTemplate secondaryMongoTemplate
 
+    @Autowired
+    JmsTemplate jmsTemplate
+
     @PostMapping("/create")
-    String create(@RequestBody Document document){
+    String create(@RequestBody Document document) {
         primaryMongoTemplate.save(document, "collection")
         secondaryMongoTemplate.save(document, "new_collection")
         return document.toJson()
+    }
+
+    @PostMapping("/message")
+    String enqueueMessage(@RequestBody String message) {
+        return jmsTemplate.convertAndSend(DESTINATION_NAME, message)
+    }
+
+    @GetMapping("/message")
+    String dequeueMessage() {
+        return jmsTemplate.receiveAndConvert(DESTINATION_NAME)
     }
 }
